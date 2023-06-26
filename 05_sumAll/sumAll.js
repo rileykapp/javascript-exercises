cab34a76c96bb5404bdd337d48c14554fe971319
rileykapp <116595317+rileykapp@users.noreply.github.com>
2023-06-25 22:10:45 -0400

    Add answers for 4 and 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let sum = 0;
+    if(x < 0 || y < 0) return "ERROR";
+    if(!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+    if(x > y){
+        let z = x;
+        x = y;
+        y = z;
+    }
+    for(let i = x; i <= y; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
